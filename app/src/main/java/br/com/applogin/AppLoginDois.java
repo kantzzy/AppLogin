e15c20a6c9180aa5e7e6623474c37f45078cfe68
kevin <kevinorigami2013@gmail.com>
2019-10-30 21:23:32 -0300

    Finalizado, porém com erros

---
+++ app/src/main/java/br/com/applogin/AppLoginDois.java
@@ -0,0 +1,14 @@
+package br.com.applogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppLoginDois extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_login_dois);
+    }
+}
